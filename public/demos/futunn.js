11548b79d65a66fbd0358f42e923ee0e62a01f7b
inu1255 <929909260@qq.com>
2019-04-17 09:46:00 +0800

    修复富途nn签到失败

--- public/demos/futunn.js
+++ public/demos/futunn.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            富途牛牛
 // @namespace       https://github.com/inu1255/soulsign-chrome
-// @version         1.0.0
+// @version         1.0.1
 // @author          inu1255
 // @loginURL        https://www.futunn.com
 // @updateURL       https://gitee.com/inu1255/soulsign-chrome/raw/master/public/demos/futunn.js
@@ -10,13 +10,27 @@
 // ==/UserScript==
 
 exports.run = async function() {
-    var { data } = await axios.get('https://www.futunn.com/site/sign-in');
+    var { data } = await axios.get('https://www.futunn.com/account/home');
+    var m = /csrf-token" content="([^"]+)/.exec(data);
+    if(!m) throw '未定位csrf';
+    var { data } = await axios.post('https://www.futunn.com/site/sign-in', '', {
+        headers: {
+            'X-CSRF-Token': m[1],
+        }
+    });
     if (data.code == 0) return `签到${data.data.signInDays}天`;
     if (data.code == 3004) return data.message;
     throw data.message;
 };
 
 exports.check = async function() {
-    var { data } = await axios.get('https://www.futunn.com/site/sign-in');
+    var { data } = await axios.get('https://www.futunn.com/account/home');
+    var m = /csrf-token" content="([^"]+)/.exec(data);
+    if(!m) throw '未定位csrf';
+    var { data } = await axios.post('https://www.futunn.com/site/sign-in', '', {
+        headers: {
+            'X-CSRF-Token': m[1],
+        }
+    });
     return data.code == 0 || data.code == 3004;
 };
