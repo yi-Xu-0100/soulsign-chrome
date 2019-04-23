194f1efecad30d8d80dc7f206b9430ad9336bbda
inu1255 <929909260@qq.com>
2019-04-23 13:10:56 +0800

    zimuzhu rename to zimuzu

---
+++ public/demos/zimuzu.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name              字幕组签到
+// @namespace         https://github.com/inu1255/soulsign-chrome
+// @version           1.0.2
+// @author            inu1255
+// @loginURL          http://www.zmz2019.com/
+// @updateURL         https://gitee.com/inu1255/soulsign-chrome/raw/master/public/demos/zimuzu.js
+// @expire            900e3
+// @domain            www.zmz2019.com
+// ==/UserScript==
+
+exports.run = async function() {
+    var { data } = await axios.get('http://www.zmz2019.com/user/login/getCurUserTopInfo', { headers: { 'Referer': 'http://www.zmz2019.com/' } });
+    if (data.status == 1) {
+        if (data.data.new_login)
+            return `签到成功,共${data.data.userinfo.point}积分`;
+        return `已签到,共${data.data.userinfo.point}积分`;
+    }
+    throw '需要登录';
+};
+
+exports.check = async function() {
+    var { data } = await axios.get('http://www.zmz2019.com/user/login/getCurUserTopInfo', { headers: { 'Referer': 'http://www.zmz2019.com/' } });
+    return data.status == 1;
+};
