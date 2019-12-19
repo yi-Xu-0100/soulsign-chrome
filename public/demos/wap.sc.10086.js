d9b33245ce88e4593b657466596fbeee5989d88e
inu1255 <929909260@qq.com>
2019-12-19 13:22:38 +0800

    2.0.1

--- public/demos/wap.sc.10086.js
+++ public/demos/wap.sc.10086.js
@@ -4,7 +4,7 @@
 // @version           1.0.1
 // @author            inu1255
 // @loginURL          https://wap.sc.10086.cn/
-// @updateURL         https://soulsign.inu1255.cn/script/inu1255/%E5%9B%9B%E5%B7%9D%E7%A7%BB%E5%8A%A8%E7%AD%BE%E5%88%B0%20%E5%A4%A7%E8%BD%AC%E7%9B%98.js
+// @updateURL         https://soulsign.inu1255.cn/script/inu1255/%E5%9B%9B%E5%B7%9D%E7%A7%BB%E5%8A%A8%E7%AD%BE%E5%88%B0+%E5%A4%A7%E8%BD%AC%E7%9B%98.js
 // @expire            900e3
 // @domain            wap.sc.10086.cn
 // @param             SSOCookie SSOCookie,通过抓包获取
