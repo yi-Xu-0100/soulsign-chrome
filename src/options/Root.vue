0dc6e6d1545b08673b948d4ef53df49af01b771f
yi_Xu <yi_Xu@yixuju.cn>
2020-06-08 21:43:01 +0800

    更新 icon 获取 api
    使用新的 api 获取网站的 icon，无法获取的会显示默认图标。

--- src/options/Root.vue
+++ src/options/Root.vue
@@ -32,7 +32,7 @@
 					</td>
 					<td>
 						<span v-for="domain in row.domains" :key="domain" :title="domain">
-							<img :src="'https://favicon.yandex.net/favicon/'+domain3(domain)" :alt="domain">
+							<img :src="'http://api.byi.pw/favicon?url='+domain3(domain)" :alt="domain">
 						</span>
 					</td>
 					<td>
