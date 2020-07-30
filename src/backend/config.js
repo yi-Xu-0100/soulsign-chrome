05af8e9f658d0aaa96d00e0df1f676e1c131f648
inu1255 <929909260@qq.com>
2020-07-30 17:55:54 +0800

    修复文件访问bug

--- src/backend/config.js
+++ src/backend/config.js
@@ -1,18 +1,28 @@
 let config = {
 	version: 10000,
-	notify_at: 0, // 通知时间
-	upgrade_at: 0, // 检查更新时间
-	notify_freq: 600, // 掉线通知频率(秒),默认10分钟
-	retry_freq: 600, // 重试间隔时间(秒),默认10分钟
-	loop_freq: 5, // 任务循环间隔时间(秒)
-	begin_at: 28800, // 每天几点开始签到,默认8点
-	upgrade: true, // 是否自动更新
-	upgrade_freq: 86400, // 检查更新间隔时间(秒)
-	timeout: 60, // 任务超时时间
+	/** 通知时间 */
+	notify_at: 0,
+	/** 检查更新时间 */
+	upgrade_at: 0,
+	/** 掉线通知频率(秒),默认10分钟 */
+	notify_freq: 600,
+	/** 重试间隔时间(秒),默认10分钟 */
+	retry_freq: 600,
+	/** 任务循环间隔时间(秒) */
+	loop_freq: 5,
+	/** 每天几点开始签到,默认8点 */
+	begin_at: 28800,
+	/** 是否自动更新 */
+	upgrade: true,
+	/** 检查更新间隔时间(秒) */
+	upgrade_freq: 86400,
+	/** 任务超时时间 */
+	timeout: 60,
 	cross: true,
 	allow_cross: {},
-	cross_header: '',
-	donate: false, // 开启捐赠, 跳转到京东返利链接
+	cross_header: "",
+	/** 开启捐赠, 跳转到京东返利链接 */
+	donate: false,
 };
 
 export default config;
--- src/backend/scriptbuild.js
+++ src/backend/scriptbuild.js
@@ -1,44 +1,45 @@
 /**
  * 生成安全的脚本
  */
-import axios from 'axios';
-import utils from './utils';
-const require_cache = {};
+import axios from "axios";
+import utils from "./utils";
 
 /**
- * 
+ *
  * @param {soulsign.Task} task 脚本允许访问的
  */
 export default function(task) {
-	let request = axios.create({ timeout: 10e3 });
+	let request = axios.create({timeout: 10e3});
 	request.interceptors.request.use(function(config) {
 		let m = /https?:\/\/([^:\/]+)/.exec(config.url);
 		if (!m) return Promise.reject(`domain配置不正确`);
-		let ss = m[1].split('.');
-		if (!task.domains.reduce(function(a, b) {
+		let ss = m[1].split(".");
+		if (
+			!task.domains.reduce(function(a, b) {
 				if (a) return true;
-				let dd = b.split('.');
+				let dd = b.split(".");
 				if (dd.length != ss.length) return false;
 				for (let i = 0; i < ss.length; i++) {
-					if (dd[i] != '*' && ss[i] != dd[i])
-						return false;
+					if (dd[i] != "*" && ss[i] != dd[i]) return false;
 				}
 				return true;
-			}, false)) return Promise.reject(`domain配置不正确`);
+			}, false)
+		)
+			return Promise.reject(`domain配置不正确`);
 		if (config.headers) {
-			if (config.headers['Referer']) {
-				config.headers['_referer'] = config.headers['Referer'];
-				delete config.headers['Referer'];
-			} else if (config.headers['referer']) {
-				config.headers['_referer'] = config.headers['referer'];
-				delete config.headers['referer'];
+			if (config.headers["Referer"]) {
+				config.headers["_referer"] = config.headers["Referer"];
+				delete config.headers["Referer"];
+			} else if (config.headers["referer"]) {
+				config.headers["_referer"] = config.headers["referer"];
+				delete config.headers["referer"];
 			}
-			if (config.headers['Origin']) {
-				config.headers['_origin'] = config.headers['Origin'];
-				delete config.headers['Origin'];
-			} else if (config.headers['origin']) {
-				config.headers['_origin'] = config.headers['origin'];
-				delete config.headers['origin'];
+			if (config.headers["Origin"]) {
+				config.headers["_origin"] = config.headers["Origin"];
+				delete config.headers["Origin"];
+			} else if (config.headers["origin"]) {
+				config.headers["_origin"] = config.headers["origin"];
+				delete config.headers["origin"];
 			}
 		}
 		return config;
@@ -48,60 +49,60 @@ export default function(task) {
 		axios: request,
 		/**
 		 * 引入第三方JS脚本
-		 * @param {string} url 
+		 * @param {string} url
 		 */
 		require(url) {
-			if (!grant.has('require')) return Promise.reject('需要@grant require');
-			if (require_cache[url])
-				return Promise.resolve(require_cache[url]);
-			return axios.get(url).then(function({ data }) {
-				let module = { exports: {} };
-				new Function('exports', 'module', data)(module.exports, module);
-				return require_cache[url] = module.exports;
+			if (!grant.has("require")) return Promise.reject("需要@grant require");
+			return axios.get(url, {validateStatus: () => true}).then(function({data}) {
+				let module = {exports: {}};
+				new Function("exports", "module", data)(module.exports, module);
+				return module.exports;
 			});
 		},
 		/**
 		 * 获取指定url指定名字的cookie
-		 * @param {string} url 
-		 * @param {string} name 
+		 * @param {string} url
+		 * @param {string} name
 		 */
 		getCookie(url, name) {
-			if (!grant.has('cookie')) return Promise.reject('需要@grant cookie');
+			if (!grant.has("cookie")) return Promise.reject("需要@grant cookie");
 			return new Promise((resolve, reject) => {
-				chrome.cookies.get({ url, name }, x => resolve(x && x.value));
+				chrome.cookies.get({url, name}, (x) => resolve(x && x.value));
 			});
 		},
 		/**
 		 * 设置指定url指定名字的cookie
-		 * @param {string} url 
-		 * @param {string} name 
-		 * @param {string} value 
+		 * @param {string} url
+		 * @param {string} name
+		 * @param {string} value
 		 */
 		setCookie(url, name, value) {
-			if (!grant.has('cookie')) return Promise.reject('需要@grant cookie');
+			if (!grant.has("cookie")) return Promise.reject("需要@grant cookie");
 			return new Promise((resolve, reject) => {
-				chrome.cookies.set({ url, name, value }, x => resolve(x && x.value));
+				chrome.cookies.set({url, name, value}, (x) => resolve(x && x.value));
 			});
 		},
 		$(html) {
-			var div = document.createElement('div')
+			var div = document.createElement("div");
 			div.innerHTML = html;
 			return div.childNodes.length > 1 ? Array.from(div.childNodes) : div.childNodes[0];
 		},
 		notify(body, url, timeout) {
-			if (!grant.has('notify')) throw '需要@grant notify';
+			if (!grant.has("notify")) throw "需要@grant notify";
 			let n = new Notification(task.name, {
 				body,
-				icon: 'https://www.google.com/s2/favicons?domain=' + task.domains[0]
+				icon: "https://www.google.com/s2/favicons?domain=" + task.domains[0],
 			});
 			n.onclick = function() {
 				this.close();
-				if (url) chrome.tabs.create({ url });
+				if (url) chrome.tabs.create({url});
 			};
-			setTimeout(function() { n.close(); }, timeout || 300e3);
+			setTimeout(function() {
+				n.close();
+			}, timeout || 300e3);
 		},
-	}
-	if (!grant.has('eval')) {
+	};
+	if (!grant.has("eval")) {
 		// 脚本中屏蔽以下内容
 		Object.assign(inject, {
 			window: undefined,
@@ -112,13 +113,13 @@ export default function(task) {
 			Function: undefined,
 			chrome: undefined,
 			globalThis: undefined,
-		})
+		});
 	}
 	let inject_keys = Object.keys(inject);
 	let inject_values = Object.values(inject);
 	task = Object.assign({}, utils.TASK_EXT, task);
-	let module = { exports: {} };
-	new Function('exports', 'module', ...inject_keys, task.code)(module.exports, module, ...inject_values);
+	let module = {exports: {}};
+	new Function("exports", "module", ...inject_keys, task.code)(module.exports, module, ...inject_values);
 	task.check = module.exports.check;
 	task.run = module.exports.run;
 	return task;
