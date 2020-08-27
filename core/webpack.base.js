93a93b370ae4fe00cfac20f8cc1b53f1f6007e05
inu1255 <929909260@qq.com>
2020-08-27 11:45:13 +0800

    处理自动刷新bug

--- core/webpack.base.js
+++ core/webpack.base.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const webpack = require('webpack')
-// const ChromeReloadPlugin  = require('wcer')
+const ChromeReloadPlugin  = require('wcer')
 const {cssLoaders, htmlPage} = require('./tools')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-
+process.on('uncaughtException', function(e) {
+  console.error(e);
+})
 let resolve = dir => path.join(__dirname, '..', 'src', dir)
 module.exports = {
   entry: {
@@ -88,10 +90,10 @@ module.exports = {
     htmlPage('魂签', 'options', ['manifest','vendor','options']),
     htmlPage('background', 'background', ['manifest','vendor','background']),
     new CopyWebpackPlugin([{ from: path.join(__dirname, '..', 'static') }]),
-    // new ChromeReloadPlugin({
-    //   port: 9090,
-    //   manifest: path.join(__dirname, '..', 'src', 'manifest.js')
-    // }),
+    new ChromeReloadPlugin({
+      port: 9090,
+      manifest: path.join(__dirname, '..', 'src', 'manifest.js')
+    }),
   ],
   performance: { hints: false },
 }
